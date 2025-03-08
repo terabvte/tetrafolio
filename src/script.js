bbbbb6b1d76b82300c69e40e53eda1fcd2a10377
terabvte <marcofediuc@gmail.com>
2025-03-08 11:44:35 +0100

    Complete other pages for the other Tetrafolio authors
    Co-authored-by: Matei Serban <Matt-Onella@users.noreply.github.com>
    Co-authored-by: Lorenzo Faccio <skyjager51@users.noreply.github.com>
    Co-authored-by: M1T730 <M1T730@users.noreply.github.com>

---
+++ src/script.js
@@ -0,0 +1,8 @@
+document.querySelector("#food-img").addEventListener("click", function () {
+  window.location.href = "https://omnifood-tera.netlify.app/";
+});
+
+document.querySelector("#pw-img").addEventListener("click", function () {
+  window.location.href =
+    "https://github.com/terabvte/web_learn?tab=readme-ov-file#password-generator";
+});
